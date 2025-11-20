bd1202daabcd7bfb5defd272bc879a9ffb0d03c4
Yeachin Arafat <yeachinarafat162@gmail.com>
2025-11-20 12:32:52 +0600

    userIdInput added

---
+++ js/login.js
@@ -0,0 +1,54 @@
+const handlelogin = async () => {
+  const userIdInput = document.getElementById("user_input_Id");
+  const passwordInput = document.getElementById("password");
+
+  const userid = userIdInput.value;
+  const password = passwordInput.value;
+
+  const user = {
+    userid: userid,
+    password: password,
+
+  };
+
+ const userInfo = await fetchUserInfo(user);
+ 
+ const errorElement =document.getElementById("user-login-error");
+
+ //user data did not match with database
+
+
+ if(userInfo.length === 0 ){
+ errorElement.classList.remove("hidden");
+ }
+ else{
+  errorElement.classList.add("hidden"); 
+
+  
+ }
+
+};
+
+const fetchUserInfo = async (user) => {
+  let data;
+  try {
+    const res = await fetch('http://localhost:5000/getUserInfo' , {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    data = await res.json();
+
+  }
+  catch (err) {
+    console.log("Error connection to the server: ", err);
+
+  }
+
+  finally {
+    return data;
+  }
+};
